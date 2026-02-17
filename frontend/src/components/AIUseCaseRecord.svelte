021be0b8548c45f0c13e05c8bb9354bed9391736
Emma Alexia <emma@modrinth.com>
2026-02-16 22:26:05 -0500

    finish section division and blanks

--- frontend/src/components/AIUseCaseRecord.svelte
+++ frontend/src/components/AIUseCaseRecord.svelte
@@ -16,9 +16,26 @@
 	const vendor_name = result.vendor_name || '[blank]'
 	const has_ato = result.has_ato || '[blank]'
 	const systems_name = result.systems_name || '[blank]'
+	const training_and_evaluation_data = result.training_and_evaluation_data || '[blank]'
+	const federal_data_catalog_link = result.federal_data_catalog_link || '[blank]'
+	const involves_pii = result.involves_pii || '[blank]'
+	const pia_link = result.pia_link || '[blank]'
+	const demographic_variables_used = result.demographic_variables_used || '[blank]'
+	const includes_custom_code = result.includes_custom_code || '[blank]'
+	const open_source_code_link = result.open_source_code_link || '[blank]'
+	const pre_deployment_testing_status = result.pre_deployment_testing_status || '[blank]'
+	const ai_impact_assessment_status = result.ai_impact_assessment_status || '[blank]'
+	const potential_impacts_description = result.potential_impacts_description || '[blank]'
+	const independent_review_status = result.independent_review_status || '[blank]'
+	const ongoing_monitoring_process = result.ongoing_monitoring_process || '[blank]'
+	const operator_training_status = result.operator_training_status || '[blank]'
+	const fail_safe_status = result.fail_safe_status || '[blank]'
+	const appeal_process_status = result.appeal_process_status || '[blank]'
+	const public_and_user_feedback = result.public_and_user_feedback || '[blank]'
 
-	const sectionTwoRequirements = result.stage_of_development !== 'Retired'
-	const sectionThreeRequirements = ['Unknown', 'Pilot', 'Deployed'].includes(result.stage_of_development)
+	const sectionTwoRequirements = stage_of_development !== 'Retired'
+	const sectionThreeRequirements = ['Unknown', 'Pilot', 'Deployed'].includes(stage_of_development)
+	const sectionFiveRequirements = ['Unknown', 'Deployed'].includes(stage_of_development) && ['High-impact', '[blank]'].includes(is_high_impact)
 </script>
 
 <details style="margin:1em 0">
@@ -27,29 +44,16 @@
 		{#if result.bureau_component}{result.bureau_component},&nbsp;{/if}{result.agency}
 	</summary>
 
+	<!--Start of section 1-->
 	<h4>Use Case Identifiers</h4>
 
-	<!--Supposed to always be filled out-->
-	<p class:blank={use_case_id === '[blank]'}>
-		<b>Internal ID:</b> {use_case_id}
-	</p>
-
-	<!--Supposed to always be filled out-->
-	<p class:blank={stage_of_development === 'Unknown'}>
-		<b>Stage of Development:</b>
-		<abbr title={stage_of_development_raw}>{stage_of_development}</abbr>
-	</p>
-
-	<!--Supposed to always be filled out-->
-	<p class:blank={is_high_impact === '[blank]'}>
-		<b>Is the AI use case high-impact?:</b> {is_high_impact}
-	</p>
+	<p class:blank={use_case_id === '[blank]'}><b>Internal ID:</b> {use_case_id}</p>
+	<p class:blank={stage_of_development === 'Unknown'}><b>Stage of Development:</b> <abbr title={stage_of_development_raw}>{stage_of_development}</abbr></p>
+	<p class:blank={is_high_impact === '[blank]'}><b>Is the AI use case high-impact?:</b> {is_high_impact}</p>
 
 	<!--Supposed to filled out if not high impact-->
 	{#if result.is_high_impact === 'Not high-impact'}
-		<p class="sub-1" class:blank={justification === '[blank]'}>
-			<b>Justification:</b> {justification}
-		</p>
+		<p class="sub-1" class:blank={justification === '[blank]'}><b>Justification:</b> {justification}</p>
 	{/if}
 
 	<!--Start of section 2-->
@@ -59,16 +63,16 @@
 	{/if}
 
 	{#if sectionTwoRequirements || use_case_topic_area !== '[blank]'}
-		<p class:blank={use_case_topic_area === '[blank]'}><b>Use Case Topic Area:</b> {result.use_case_topic_area || '[blank]'}</p>
+		<p class:blank={use_case_topic_area === '[blank]'}><b>Use Case Topic Area:</b> {use_case_topic_area}</p>
 	{/if}
 	{#if sectionTwoRequirements || ai_classification !== '[blank]'}
-		<p class:blank={ai_classification === '[blank]'}><b>AI Classification:</b> {result.ai_classification || '[blank]'}</p>
+		<p class:blank={ai_classification === '[blank]'}><b>AI Classification:</b> {ai_classification}</p>
 	{/if}
 	{#if sectionTwoRequirements || problem_statement !== '[blank]'}
-		<p class:blank={problem_statement === '[blank]'}><b>What problem is the AI intended to solve?:</b> {result.problem_statement || '[blank]'}</p>
+		<p class:blank={problem_statement === '[blank]'}><b>What problem is the AI intended to solve?:</b> {problem_statement}</p>
 	{/if}
 	{#if sectionTwoRequirements || expected_benefits !== '[blank]'}
-		<p class:blank={expected_benefits === '[blank]'}><b>What are the expected benefits and positive outcomes from the AI for an agency's mission and/or the general public?:</b> {result.expected_benefits || '[blank]'}</p>
+		<p class:blank={expected_benefits === '[blank]'}><b>What are the expected benefits and positive outcomes from the AI for an agency's mission and/or the general public?:</b> {expected_benefits}</p>
 	{/if}
 	{#if sectionTwoRequirements || system_outputs !== '[blank]'}
 		<p class:blank={system_outputs === '[blank]'}><b>Describe the AI system’s outputs:</b> {system_outputs}</p>
@@ -100,78 +104,62 @@
 	{#if sectionThreeRequirements}
 		<br>
 		<h4>Data and Code</h4>
-		<!--TODO unfinished-->
 	{/if}
 
-	{#if result.stage_of_development !== 'Unknown'}
-		<ul>
-			{#if result.training_and_evaluation_data}
-				<li><b>Describe any data used to train, fine-tune, and/or evaluate performance of the model(s) used in this use case:</b> {result.training_and_evaluation_data}</li>
-			{/if}
-			{#if result.federal_data_catalog_link}
-				<li><b>If the data is required to be publicly disclosed as an open government data asset, provide a link to the entry on the Federal Data Catalog:</b> {result.federal_data_catalog_link}</li>
-			{/if}
-			{#if result.involves_pii}
-				<li><b>Does this AI use case involve personally identifiable information (PII) that is maintained by the agency?:</b> {result.involves_pii}</li>
-			{/if}
-			{#if result.pia_link}
-				<li><b>If publicly available, provide the link to the AI use case's associated Privacy Impact Assessment (PIA):</b>
-					{#if result.pia_link.substring(0,4)==='http'}
-						<a href={result.pia_link} target="_blank">{result.pia_link}</a>
-					{:else}
-						{result.pia_link}
-					{/if}
-				</li>
-			{/if}
-			{#if result.demographic_variables_used}
-				<li><b>Which, if any, demographic variables does the AI use case explicitly use as model features?:</b> {result.demographic_variables_used}</li>
-			{/if}
-			{#if result.includes_custom_code}
-				<li><b>Does this project include custom-developed code?:</b> {result.includes_custom_code}</li>
-			{/if}
-			{#if result.open_source_code_link}
-				<li><b>If the code is open source, provide the link for the publicly available source code:</b>
-					{#if result.open_source_code_link.substring(0,4)==='http'}
-						<a href={result.open_source_code_link} target="_blank">{result.open_source_code_link}</a>
-						{:else}
-						{result.open_source_code_link}
-					{/if}
-				</li>
-			{/if}
-			{#if result.pre_deployment_testing_status}
-				<li><b>Has pre-deployment testing been conducted?:</b> {result.pre_deployment_testing_status}</li>
-			{/if}
-			{#if result.ai_impact_assessment_status}
-				<li><b>Has an AI impact assessment been completed?:</b>
-					{#if result.ai_impact_assessment_status.substring(0,4)==='http'}
-						<a href={result.ai_impact_assessment_status} target="_blank">{result.ai_impact_assessment_status}</a>
-					{:else}
-						{result.ai_impact_assessment_status}
-					{/if}
-				</li>
-			{/if}
-			{#if result.potential_impacts_description}
-				<li><b>What are the potential impacts of using the AI for this particular use case and how were they identified?:</b> {result.potential_impacts_description}</li>
-			{/if}
-			{#if result.independent_review_status}
-				<li><b>Has an independent review of the AI use case been conducted?:</b> {result.independent_review_status}</li>
-			{/if}
-			{#if result.ongoing_monitoring_process}
-				<li><b>Is there a process to conduct ongoing monitoring to identify any adverse impacts to the performance and security of the AI functionality, as well as to privacy, civil rights, and civil liberties?:</b> {result.ongoing_monitoring_process}</li>
-			{/if}
-			{#if result.operator_training_status}
-				<li><b>Has the agency established sufficient and periodic training for operators of the AI to interpret and act on the its output and managed associated risks?:</b> {result.operator_training_status}</li>
-			{/if}
-			{#if result.fail_safe_status}
-				<li><b>Does this AI use case have an appropriate fail-safe that minimizes the risk of significant harm?:</b> {result.fail_safe_status}</li>
-			{/if}
-			{#if result.appeal_process_status}
-				<li><b>Is there an established appeal process in the event that an impacted individual would like to appeal or contest the AI system's outcome?:</b> {result.appeal_process_status}</li>
-			{/if}
-			{#if result.public_and_user_feedback}
-				<li><b>What steps has the agency taken to consult and incorporate feedback from end users of this AI use case and the public?:</b> {result.public_and_user_feedback}</li>
-			{/if}
-		</ul>
+	{#if sectionThreeRequirements || training_and_evaluation_data !== '[blank]'}
+		<p class:blank={training_and_evaluation_data === '[blank]'}><b>Describe any data used to train, fine-tune, and/or evaluate performance of the model(s) used in this use case:</b> {training_and_evaluation_data}</p>
+	{/if}
+	{#if sectionThreeRequirements || federal_data_catalog_link !== '[blank]'}
+		<p class:blank={federal_data_catalog_link === '[blank]'}><b>If the data is required to be publicly disclosed as an open government data asset, provide a link to the entry on the Federal Data Catalog:</b> {federal_data_catalog_link}</p>
+	{/if}
+	{#if sectionThreeRequirements || involves_pii !== '[blank]'}
+		<p class:blank={involves_pii === '[blank]'}><b>Does this AI use case involve personally identifiable information (PII) that is maintained by the agency?:</b> {involves_pii}</p>
+	{/if}
+	{#if sectionThreeRequirements || pia_link !== '[blank]'}
+		<p class:blank={pia_link === '[blank]'}><b>If publicly available, provide the link to the AI use case's associated Privacy Impact Assessment (PIA):</b> {pia_link}</p>
+	{/if}
+	{#if sectionThreeRequirements || demographic_variables_used !== '[blank]'}
+		<p class:blank={demographic_variables_used === '[blank]'}><b>Which, if any, demographic variables does the AI use case explicitly use as model features?:</b> {demographic_variables_used}</p>
+	{/if}
+	{#if sectionThreeRequirements || includes_custom_code !== '[blank]'}
+		<p class:blank={includes_custom_code === '[blank]'}><b>Does this project include custom-developed code?:</b> {includes_custom_code}</p>
+	{/if}
+	{#if sectionThreeRequirements || open_source_code_link !== '[blank]'}
+		<p class:blank={open_source_code_link === '[blank]'}><b>If the code is open source, provide the link for the publicly available source code:</b> {open_source_code_link}</p>
+	{/if}
+
+	<!--Start of section 5-->
+	{#if sectionFiveRequirements}
+		<br>
+		<h4>Risk Management</h4>
+	{/if}
+
+	{#if sectionFiveRequirements || pre_deployment_testing_status !== '[blank]'}
+		<p class:blank={pre_deployment_testing_status === '[blank]'}><b>Has pre-deployment testing been conducted?:</b> {pre_deployment_testing_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || ai_impact_assessment_status !== '[blank]'}
+		<p class:blank={ai_impact_assessment_status === '[blank]'}><b>Has an AI impact assessment been completed?:</b> {ai_impact_assessment_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || potential_impacts_description !== '[blank]'}
+		<p class:blank={potential_impacts_description === '[blank]'}><b>What are the potential impacts of using the AI for this particular use case and how were they identified?:</b> {potential_impacts_description}</p>
+	{/if}
+	{#if sectionFiveRequirements || independent_review_status !== '[blank]'}
+		<p class:blank={independent_review_status === '[blank]'}><b>Has an independent review of the AI use case been conducted?:</b> {independent_review_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || ongoing_monitoring_process !== '[blank]'}
+		<p class:blank={ongoing_monitoring_process === '[blank]'}><b>Is there a process to conduct ongoing monitoring to identify any adverse impacts to the performance and security of the AI functionality, as well as to privacy, civil rights, and civil liberties?:</b> {ongoing_monitoring_process}</p>
+	{/if}
+	{#if sectionFiveRequirements || operator_training_status !== '[blank]'}
+		<p class:blank={operator_training_status === '[blank]'}><b>Has the agency established sufficient and periodic training for operators of the AI to interpret and act on the its output and managed associated risks?:</b> {operator_training_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || fail_safe_status !== '[blank]'}
+		<p class:blank={fail_safe_status === '[blank]'}><b>Does this AI use case have an appropriate fail-safe that minimizes the risk of significant harm?:</b> {fail_safe_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || appeal_process_status !== '[blank]'}
+		<p class:blank={appeal_process_status === '[blank]'}><b>Is there an established appeal process in the event that an impacted individual would like to appeal or contest the AI system's outcome?:</b> {appeal_process_status}</p>
+	{/if}
+	{#if sectionFiveRequirements || public_and_user_feedback !== '[blank]'}
+		<p class:blank={public_and_user_feedback === '[blank]'}><b>What steps has the agency taken to consult and incorporate feedback from end users of this AI use case and the public?:</b> {public_and_user_feedback}</p>
 	{/if}
 </details>
 
@@ -196,7 +184,7 @@
 	}
 
 	details > p {
-		margin-bottom: 0;
+		margin-bottom: .25em;
 	}
 
 	details > p:last-child {
@@ -204,7 +192,7 @@
 	}
 
 	.sub-1 {
-		margin-left: 1rem;
+		margin-left: 2rem;
 	}
 
 	.blank {
