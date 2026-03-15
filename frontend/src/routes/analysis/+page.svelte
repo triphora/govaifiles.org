e32c3cfad09a7264be8db7c72753e50345700383
Emma Alexia <emma@modrinth.com>
2026-03-14 23:21:29 -0400

    Rename "Analysis" to "Graphs" since no actual analysis yet

--- frontend/src/routes/analysis/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-</script>
-
-<h1>Analysis</h1>
-
-<iframe title="Graph" src="https://govaifiles.org/plotly" width=100% height=600></iframe>
---
+++ frontend/src/routes/graphs/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<h1>Graphs</h1>
+
+<iframe title="Interactive Graph" src="https://govaifiles.org/plotly" width=100% height=600></iframe>
