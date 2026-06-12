ec9a637a4820a0ee6900a71d964205c98ff7bcb4
Emma Fauser <efauser@haverford.edu>
2026-06-12 16:01:31 -0400

    Add identified discrepancies to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -23,4 +23,31 @@
 	<p><strong><a href="https://fauser.dev" target="_blank">Emma Fauser</a></strong> is an undergraduate student at Haverford College pursuing computer science and urban studies. Her interests broadly lie in the applied use of computing for social problems. She is also a founding employee at Modrinth, a startup supporting Minecraft content creators.</p>
 
 	<p><strong><a href="https://hqselene.github.io" target="_blank">Qing He</a></strong> is a graduate student at the University of Pennsylvania with a background in Computer Science, Finance, and Architecture. Her research sits at the intersection of human-AI interaction and cognitive augmentation, with a focus on designing tools that support complex decision-making and knowledge work.</p>
+
+	<h2>Identified Discrepancies</h2>
+
+	<p>This section intends to list all of the known discrepancies between our merged data (as used on this website) and the official OMB consolidated file for 2025. We hope that these discrepancies can be resolved in a later revision, either published by the individual agencies or by the OMB.</p>
+
+	<ul>
+		<li>The OMB file has five (5) extra entries that were not present in the individual agency files, and thus are not present in the search interface. For posterity, these extra entries are hosted <a target="_blank" href="/2025_omb_extra_entries.csv">here</a>.</li>
+		<ul>
+			<li>Department of Homeland Security: two (2) extra entries: DHS-9 and DHS-2544. Both are classified as retired and not high impact.</li>
+			<li>National Credit Union Administration: three (3) extra entries: NCUA-12-2025, NCUA-13-2025, and NCUA-14-2025. All three are classified as deployed and high impact.</li>
+		</ul>
+		<li>The OMB file is missing at least twenty-five (25) entries:</li>
+		<ul>
+			<li>
+				National Aeronautics and Space Administration: four (4) entries: <a target="_blank" href="/?q=NASA-96#use-case-2025-national-aeronautics-and-space-administration-nasa-96-development-of-a-benchmark-training-set-to-support-an-autonomous-minion">NASA-96</a>, <a target="_blank" href="/?q=NASA-532&yearFrom=2025#use-case-2025-national-aeronautics-and-space-administration-nasa-532-initial-outline-and-partial-first-draft-of-a-nasa-aeronautics-technical-challenge-tc">NASA-532</a>, <a target="_blank" href="/?q=NASA-859#use-case-2025-national-aeronautics-and-space-administration-nasa-859-gcmd-keyword-recommender-gkr">NASA-859</a>, and <a target="_blank" href="/?q=NASA-926#use-case-2025-national-aeronautics-and-space-administration-nasa-926-clara">NASA-926</a>
+			</li>
+			<li><a target="_blank" href="/?agency=Department+of+Education&yearFrom=2025">Department of Education</a>: twenty-one (21) entries, alongside most of the data fields supplied by the agency.</li>
+		</ul>
+		<li>The OMB file has twenty-seven (27) entries that were supposed to be withheld, either due to a disclosure risk or because disclosure is prohibited by law. Eighteen (18) of these were not present in the individual agency files, and thus are not present in the search interface. For posterity, these withheld entries are hosted <a target="_blank" href="/2025_withheld_entries.csv">here</a>.</li>
+		<ul>
+			<li>National Aeronautics and Space Administration: fifteen (15) entries that were supposed to be withheld.</li>
+			<li>Environmental Protection Agency: three (3) entries that were supposed to be withheld due to a disclosure risk were included in the OMB file.</li>
+			<li>Nine (9) other entries that are present in the search interface from the Department of Labor, Federal Energy Regulatory Commission, and National Archives and Records Administration.</li>
+		</ul>
+		<li>The OMB file has entry <a target="_blank" href="/?q=DHS-2552#use-case-2025-department-of-homeland-security-dhs-2552-mobile-device-forensics-for-investigations">DHS-2552</a> under "Pre-deployment" status instead of "Deployed", and is also missing several of the agency data fields.</li>
+	</ul>
+	<p>If you find more discrepancies, please notify us using the contact email above.</p>
 </section>
